390ec129735570df6e8b1f483af2758e3ade960f
Jason Firkus <jacecraft29@gmail.com>
2025-02-19 13:18:52 -0800

    update

---
+++ src/scripts/login.js
@@ -0,0 +1,25 @@
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function () {
+      document.getElementById("loader").style.display = "none";
+    },
+  },
+  signInFlow: "popup",
+  signInSuccessUrl: "main.html",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  tosUrl: "<your-tos-url>",
+  privacyPolicyUrl: "<your-privacy-policy-url>",
+};
+
+ui.start("#firebaseui-auth-container", uiConfig);
