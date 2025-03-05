24afa4854cc9bfacfb2f5a40cad0fa514a3c0d50
Jason Firkus <jacecraft29@gmail.com>
2025-03-05 13:55:42 -0800

    update auth

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -6,9 +6,7 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 const uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
+      addUserToDB(authResult, redirectUrl);
       return true;
     },
     uiShown: () => {},
@@ -22,5 +20,34 @@ const uiConfig = {
   tosUrl: "<your-tos-url>",
   privacyPolicyUrl: "<your-privacy-policy-url>",
 };
+
+async function addUserToDB(authResult, redirectUrl) {
+  const userAuth = authResult.user;
+  const userDoc = db.collection("users").doc(userAuth.uid);
+  await fetch("/serverlog", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authResult),
+  });
+
+  // try {
+  //   //if the userDoc doesn't exist, create it
+  //   if (!(await userDoc.get().exists)) {
+  //     await userDoc.set({
+  //       email: userAuth.email,
+  //       displayName: userAuth.displayName || "",
+  //       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  //     });
+  //     console.log("User document created");
+  //   } else {
+  //     console.log("User document already exists");
+  //   }
+  // } catch (error) {
+  //   console.error("Error creating user document:", error);
+  // }
+  return true;
+}
 
 ui.start("#firebase-ui-auth-container", uiConfig);
