a86f5003cbfe61f8273b315669ec82238d58bddc
Jason Firkus <jacecraft29@gmail.com>
2025-03-11 16:50:44 -0700

    remove client side auth

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -35,39 +35,4 @@ const uiConfig = {
   privacyPolicyUrl: "<your-privacy-policy-url>",
 };
 
-function addUserToDB(authResult) {
-  const userAuth = authResult.user;
-  const userDoc = db.collection("users").doc(userAuth.uid);
-  await fetch("/serverlog", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(authResult),
-  });
-
-  try {
-    //if the userDoc doesn't exist, create it
-    if (!(userDoc.get().exists)) {
-       userDoc.set({
-        email: userAuth.email,
-        displayName: userAuth.displayName || "",
-        createdAt: firebase.firestore.FieldValue.serverTimestamp()
-      }).then(function () {
-        console.log("New user added to firestore");
-        window.location.assign("/html/home.html");       //re-direct to main.html after signup
-      }).catch(function (error) {
-        console.log("Error adding new user: " + error);
-      });
-      console.log("User document created");
-     } else {
-        console.log("User document alteady exists");
-        return true;
-    }
-        return false;
-    } catch (error) {
-    console.error("Error creating user document:", error);
-  }
-}
-
 ui.start("#firebase-ui-auth-container", uiConfig);
