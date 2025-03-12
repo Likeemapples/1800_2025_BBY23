fb746509052985b335c9cad698940698ee4804d8
Jason Firkus <jacecraft29@gmail.com>
2025-03-12 12:36:50 -0700

    add ecoaction to user

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,24 @@ import { db, admin, app } from "../config/firebase.js";
 
 const router = Router();
 
+async function authenticateToken(request, response, next) {
+  const authHeader = request.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return response.status(401).send("Unauthorized: No token provided");
+  }
+
+  const idToken = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    request.user = decodedToken;
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    response.status(401).send("Unauthorized: Invalid token");
+  }
+}
+
 app.post("/", async (request, response) => {
   const { user: userAuth } = request.body.authResult;
   console.log("userAuth.uid", userAuth.uid);
@@ -29,7 +47,33 @@ app.post("/", async (request, response) => {
 });
 
 app.put("/", (request, response) => {
-  // update user
+  // update user settings
+});
+
+app.post("/ecoaction", authenticateToken, async (request, response) => {
+  // add ecoaction to ecoactions collection in user
+  const { ecoactionID } = request.body;
+  const { uid: userID } = request.user;
+
+  try {
+    const response = await db
+      .collection("users")
+      .doc(userID)
+      .collection("ecoactions")
+      .doc(ecoactionID)
+      .set({});
+    console.log("response", response);
+    response.status(200).send("Ecoaction successfully added to user");
+  } catch (error) {
+    console.log(`${error.name} adding ecoaction to user ${userID}`, error);
+    response
+      .status(500)
+      .json({ message: `${error.name} adding ecoaction to user ${userID}`, error });
+  }
+});
+
+app.post("/ecogroup", (request, response) => {
+  // add ecogroup to ecogroup collection in user
 });
 
 export default router;
