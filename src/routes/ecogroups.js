ed2cbdcff7f085d21ab663067c4f080496af6226
Hazel <fulljamesisaku@gmail.com>
2025-03-15 11:59:33 -0700

    Code clean + group page

--- src/routes/ecogroups.js
+++ src/routes/ecogroups.js
@@ -44,28 +44,6 @@ router.post("/create", authenticateToken, async (request, response) => {
         });
       }
     });
-
-  // const userDoc = db.collection("ecogroups").doc(userID);
-
-  // try {
-  //   //if the userDoc doesn't exist, create it
-  //   if (!(await userDoc.get()).exists) {
-  //     await userDoc.set({
-  //       email: userAuth.email,
-  //       displayName: userAuth.displayName || "",
-  //       createdAt: admin.firestore.FieldValue.serverTimestamp(),
-  //     });
-  //     console.log("User document created");
-  //     response.status(200).send("User document created");
-  //   } else {
-  //     console.log("User document already exists");
-  //     response.status(200).send("User document already exists");
-  //   }
-  // } catch (error) {
-  //   console.error("Error creating user document:", error);
-  //   response.status(200).json({ message: "Error creating user document", error });
-  // }
-
 });
 
 router.post("/member", (request, response) => {
