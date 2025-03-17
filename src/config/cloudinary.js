28def0727efc4d56357e348a7a013814b4fe047a
Jason Firkus <jacecraft29@gmail.com>
2025-03-17 13:12:52 -0700

    fix daily challenges

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,8 +1,6 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import { cloudinaryConfig } from "./auth.js";
 
-cloudinary.config(cloudinaryConfig); 
+cloudinary.config(cloudinaryConfig);
 
-
-    
-  export { cloudinary };
+export { cloudinary };
