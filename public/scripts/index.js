59972b0c10a0ecd357d51bea9d2e0f70f2397847
jason <jacecraft29@gmail.com>
2025-04-06 03:01:43 -0700

    update welcome page gradient background, make fonts respnsive, update login styling, make logo clickable

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,29 @@
-import Granim from "https://cdn.skypack.dev/granim";
-
-new Granim({
-  element: "#granimCanvas1",
-  direction: "left-right",
-  states: {
-    "default-state": {
-      gradients: [
-        // ["#b2f0ca", "#44c732", "#28791b"],
-        // ["#28791b", "#44c732", "#b2f0ca"],
-        [
-          { color: "#daf6d5", pos: 0.2 },
-          { color: "#8fe382", pos: 0.8 },
-          { color: "#36a725", pos: 1 },
-        ],
-        [
-          { color: "#36a725", pos: 0 },
-          { color: "#8fe382", pos: 0.2 },
-          { color: "#daf6d5", pos: 0.8 },
-        ],
-      ],
-      transitionSpeed: 6000,
-      loop: true,
+new FinisherHeader({
+  count: 10,
+  size: {
+    min: 400,
+    max: 1300,
+    pulse: 0,
+  },
+  speed: {
+    x: {
+      min: 0.1,
+      max: 0.2,
+    },
+    y: {
+      min: 0.1,
+      max: 0.2,
     },
   },
+  colors: {
+    background: "#daf6d5",
+    particles: ["#3fbd28", "#7de06c", "#c7f2c0"],
+  },
+  blending: "overlay",
+  opacity: {
+    center: 1,
+    edge: 0.15,
+  },
+  skew: 0,
+  shapes: ["c"],
 });
